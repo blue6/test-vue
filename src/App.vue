3f7697d73f4123463053d2b9185777ec878d988e
blue6 <blue6@blue6-no-MacBook-Pro.local>
2021-11-14 03:02:39 +0900

    add ZZZ

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App !!!XXXYYY"/>
+    <HelloWorld msg="Welcome to Your Vue.js App !!!XXXYYYZZZ"/>
   </div>
 </template>
 
